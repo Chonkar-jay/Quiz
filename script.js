1fe188d25a4d4c907287c91100d802f98e486a66
Chonkar-jay <120003007+Chonkar-jay@users.noreply.github.com>
2023-03-18 10:10:01 +0530

    GIT

--- script.js
+++ script.js
@@ -44,66 +44,162 @@ function generateQuiz(question , astronomyQueContainer , finishBtnContainer , re
             answer = [];
 
             // for each available answer...
-            for(letter in ques[i].answers)
+            for(letter in ques[i].answer)
             {
                 // ...add an html radio button
-                answers.push(
+                answer.push(
                 '<label>' + '<input type="radio" name="ques'+i+'" value="' +letter+'">' + letter + ': ' + question[i].answer[letter] + '</label>'
                 );
             }
             // add this question and its answers to the output
-            output.push('<div class="question">' + question[i].question + '</div>' + '<div class="answers">' + answers.join('') + '</div>');
+            output.push('<div class="question">' + question[i].question + '</div>' + '<div class="answer">' + answer.join('') + '</div>');
         }
         // finally combine our output list into one string of html and put it on the page
         astronomyQueContainer.innerHTML = output.join('');
+
+        function showResults(question, astronomyQueContainer, resultContainer)
+        {
+            // gather answer containers from our quiz
+            var answerContainers = quizContainer.querySelectorAll('.answer');
+            
+            // keep track of user's answers
+            var userAnswer = '';
+            var numCorrect = 0;
+    
+            // for each question...
+            for(var i=0; i<question.length; i++)
+            {
+
+                // find selected answer
+                userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+      
+                // if answer is correct
+                if(userAnswer===question[i].correctAnswer)
+                {
+                    // add to the number of correct answers
+                    numCorrect++;
+        
+                    // color the answers green
+                    answerContainers[i].style.color = 'lightgreen';
+                }
+                // if answer is wrong or blank
+                else
+                {
+                    // color the answers red
+                    answerContainers[i].style.color = 'red';
+                }
+            }
+
+            // show number of correct answers out of total
+            resultsContainer.innerHTML = numCorrect + ' out of ' + question.length;
+        }
+
+        // show questions right away
+        showQuestions(question, astronomyQueContainer);
+  
+        // on submit, show results
+        submitButton.onclick = function()
+        {
+            showResults(question, astronomyQueContainer, resultsContainer);
+        }
+
     }
+
+
+            
 }
 
 
 
 
-
-
-// function showResults(questions, quizContainer, resultsContainer){
+// const quizDB = [
+//     {
+//         question: "Q1: * ",
+//         a: "a )",
+//         b: "b )",
+//         c: "c )",
+//         d: "d )",
+//         ans: "ans4"
+//     },
+//     {
+//         question: "Q2: ** ",
+//         a: "a )",
+//         b: "b )",
+//         c: "c )",
+//         d: "d )",
+//         ans: "ans4"
+//     },
+//     {
+//         question: "Q3: *** ",
+//         a: "a )",
+//         b: "b )",
+//         c: "c )",
+//         d: "d )",
+//         ans: "ans4"
+//     },
+//     {
+//         question: "Q4: **** ",
+//         a: "a )",
+//         b: "b )",
+//         c: "c )",
+//         d: "d )",
+//         ans: "ans4"
+//     },
+//     {
+//         question: "Q5: ***** ",
+//         a: "a )",
+//         b: "b )",
+//         c: "c )",
+//         d: "d )",
+//         ans: "ans4"
+//     }
+// ];
+// const question = document.querySelector('.question');
+// const option1 = document.querySelector('#option1');
+// const option2 = document.querySelector('#option2');
+// const option3 = document.querySelector('#option3');
+// const option4 = document.querySelector('#option4');
+// const submit = document.querySelector('#submit');
+// const answers = document.querySelectorAll('.answer');
+// let questionNum = 0;
+// let score = 0;
+// const loadQuestion = () => {
+//     const questionList = quizDB[questionNum];
     
-//     // gather answer containers from our quiz
-//     var answerContainers = quizContainer.querySelectorAll('.answers');
-    
-//     // keep track of user's answers
-//     var userAnswer = '';
-//     var numCorrect = 0;
-    
-//     // for each question...
-//     for(var i=0; i<questions.length; i++){
+//     question.innerText = questionList.question;
+//     option1.innerText = questionList.a;
+//     option2.innerText = questionList.b;
+//     option3.innerText = questionList.c;
+//     option4.innerText = questionList.d;
+// }
+// loadQuestion();
+// const getcheckAns = () => {
+//     let answer;
+//     answers.forEach((curAnsElem) => {
+//         if(curAnsElem.checked){
+//             answer = curAnsElem.id;
+//         }
+//     });
+//     return answer;
+// }
+// submit.addEventListener('click', () => {
+//     const checkAns = getcheckAns();
+//     console.log(checkAns);
 
-//       // find selected answer
-//       userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
-      
-//       // if answer is correct
-//       if(userAnswer===questions[i].correctAnswer){
-//         // add to the number of correct answers
-//         numCorrect++;
-        
-//         // color the answers green
-//         answerContainers[i].style.color = 'lightgreen';
-//       }
-//       // if answer is wrong or blank
-//       else{
-//         // color the answers red
-//         answerContainers[i].style.color = 'red';
-//       }
+//     if(checkAns === quizDB[questionNum].ans){
+//         score++;
+//     };
+
+//     questionNum++;
+//     if(questionNum < quizDB.length){
+//         loadQuestion();
 //     }
 
-//     // show number of correct answers out of total
-//     resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
-//   }
+// })
 
-//   // show questions right away
-//   showQuestions(questions, quizContainer);
-  
-//   // on submit, show results
-//   submitButton.onclick = function(){
-//     showResults(questions, quizContainer, resultsContainer);
-//   }
 
-// }
+
+    
+    
+    
+    
